49aab452dc34b5aeeee8c5d172688e89f2e1e9dc
Federico Bonfiglio <federico.bonfiglio@protonmail.ch>
2022-08-10 10:44:36 +0200

    better layout

--- src/components/Home.js
+++ src/components/Home.js
@@ -121,53 +121,15 @@ const Home = ({ user }) => {
                 <div className={searchClassName}>
                     <Search 
                         filterAttribute={filterAttribute} 
+                        setFilterAttribute={setFilterAttribute}
                         filterTextRef={filterTextRef}
                         filterScores={filterScores}
                         isChangingFilter={isChangingFilter}
                         setChangingFilter={setChangingFilter}
                     ></Search>
                 </div>
-                {/* { !isAddingScore?
-                <div className={"md:hidden"}>
-                    <Search 
-                        filterAttribute={filterAttribute} 
-                        filterTextRef={filterTextRef}
-                        filterScores={filterScores}
-                        isChangingFilter={isChangingFilter}
-                        setChangingFilter={setChangingFilter}
-                    ></Search>
-                </div>
-                :
-                <div></div>
-                } */}
 
-                { isChangingFilter? 
-                    <div className={"flex flex-col"}>
-                        <button 
-                        className={ 
-                            "flex justify-center py-4 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue active:bg-blue-700 transition duration-150 ease-in-out"
-                        }
-                        onClick={() => {
-                                setFilterAttribute('title');
-                                setChangingFilter(false);
-                            }
-                        }>
-                            Title
-                        </button>
-                        <button 
-                        className={ 
-                            "flex justify-center py-4 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue active:bg-blue-700 transition duration-150 ease-in-out"
-                        }
-                        onClick={() => {
-                                setFilterAttribute('author');
-                                setChangingFilter(false);
-                            }
-                        }>
-                            Author
-                        </button>
-                    </div> 
-                    : <div></div>}
-                {(!isChangingFilter && ! isAddingScore)?
+                {!isAddingScore?
                     <div className="md:hidden">
                     <ScoreList scores={scores} setScores={setScores} setError={setError}></ScoreList>
                     </div>
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,25 +1,63 @@
 
-const Search = ({ filterAttribute, filterTextRef, filterScores, isChangingFilter, setChangingFilter }) => {
+const Search = ({ 
+    filterAttribute,
+    setFilterAttribute, 
+    filterTextRef, 
+    filterScores, 
+    isChangingFilter, 
+    setChangingFilter }) => {
 
     return (
-        <div className={"columns-2"}>
-            <p>Search: </p>
-            <input
-                    ref={filterTextRef}
-                    type="text"
-                    onKeyUp={(e) => filterScores()}
-                    className={
-                        "bg-gray-200 border px-2 border-gray-300 w-full mr-4"
-                    }
-                />
+        <div>
+            <p className={"py-2"}>Search: </p>
+            <div className={"columns-2"}>
+                <div className={" py-1"}>
+                    <input
+                            ref={filterTextRef}
+                            type="text"
+                            onKeyUp={(e) => filterScores()}
+                            className={
+                                "bg-gray-200 border px-2 border-gray-300 w-full mr-4"
+                            }
+                        />
+                </div>
+                <div className={"columns-4"}>
+                    <button
+                        onClick={() => setChangingFilter(!isChangingFilter)}
+                        className={
+                            "flex justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue active:bg-blue-700 transition duration-150 ease-in-out"
+                        }
+                    >{filterAttribute.replace(/^\w/, (c) => c.toUpperCase())}</button>
 
-            <button
-                onClick={() => setChangingFilter(!isChangingFilter)}
-                className={
-                    "flex justify-center py-4 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue active:bg-blue-700 transition duration-150 ease-in-out"
-                }
-            >{filterAttribute.replace(/^\w/, (c) => c.toUpperCase())}</button>
-
+                
+                { isChangingFilter? 
+                    <div className={""}>
+                        <button 
+                        className={ 
+                            "flex justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-500 focus:outline-none focus:border-red-700 focus:shadow-outline-blue active:bg-red-700 transition duration-150 ease-in-out"
+                        }
+                        onClick={() => {
+                                setFilterAttribute('title');
+                                setChangingFilter(false);
+                            }
+                        }>
+                            Title
+                        </button>
+                        <button 
+                        className={ 
+                            "flex justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-500 focus:outline-none focus:border-red-700 focus:shadow-outline-blue active:bg-red-700 transition duration-150 ease-in-out"
+                        }
+                        onClick={() => {
+                                setFilterAttribute('author');
+                                setChangingFilter(false);
+                            }
+                        }>
+                            Author
+                        </button>
+                    </div> 
+                    : <div></div>}
+                                    </div>
+            </div>
         </div>
     );
 };
